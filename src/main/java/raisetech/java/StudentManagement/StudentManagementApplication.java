800f14fd064f293920fe9cdd337147e8f459ac01
KeisukeFuruta <kcomethunter@gmail.com>
2024-07-07 13:37:02 +0900

    課題：17回課題への取り組み

--- src/main/java/raisetech/java/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/java/StudentManagement/StudentManagementApplication.java
@@ -1,22 +1,30 @@
 package raisetech.java.StudentManagement;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Enami Kouji";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello, World";
+	@GetMapping("/name")
+	public String getName(){
+		return name;
+	}
+
+	@PostMapping("/name")
+	public void setName(String name){
+		this.name = name;
 	}
 
 }
