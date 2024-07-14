90a3399eb9118fc90ec6958b1597eb8c99eb01b3
KeisukeFuruta <kcomethunter@gmail.com>
2024-07-14 16:24:49 +0900

    Mapの実装並びにPOSTして値の書き換え

--- src/main/java/raisetech/java/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/java/StudentManagement/StudentManagementApplication.java
@@ -1,8 +1,9 @@
 package raisetech.java.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,28 +12,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentManagementApplication {
 
-	private String name = "Enami Kouji";
-	private String age = "37";
+  private Map<Integer, String> studentInfo;
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
+  public StudentManagementApplication() {
+    this.studentInfo = new HashMap<>();
+    studentInfo.put(23, "Yui Yamazoe");
+    studentInfo.put(29, "Keisuke Furuta");
+    studentInfo.put(27, "Kouhei Matsunaga");
+  }
 
-	@GetMapping("/studentInfo")
-	public String getStudentInfo(){
-		return name + " " + age + "歳";
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementApplication.class, args);
+  }
 
+  @GetMapping("/studentInfo")
+  public Map<Integer, String> getStudentInfo() {
+    return studentInfo;
+  }
 
-
-	@PostMapping("/studentInfo")
-	public void setStudentInfo(String name , String age){
-		this.name = name;
-		this.age = age;
-	}
-
-	@PostMapping("/studentName")
-	public void updateStudentName(String name){
-		this.name = name;
-	}
+  @PostMapping("/updateStudentInfo")
+  public String updateStudentInfo(Integer age, String name) {
+    if (studentInfo.containsKey(age)) {
+      studentInfo.put(age, name);
+      return "学生情報をアップデートしました[ " + age + " - " + name + " ]";
+    } else {
+      return "学生情報が見つかりません [" + age + " ]";
+    }
+  }
 }
+
+
+
