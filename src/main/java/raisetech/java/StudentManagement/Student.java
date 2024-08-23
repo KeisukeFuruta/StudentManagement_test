6b2a83f4c08be0e332ed2c78efd601a13596ca66
KeisukeFuruta <kcomethunter@gmail.com>
2024-08-23 12:36:27 +0900

    課題：23回課題への取り組み

--- src/main/java/raisetech/java/StudentManagement/Student.java
+++ src/main/java/raisetech/java/StudentManagement/Student.java
@@ -1,23 +1,21 @@
 package raisetech.java.StudentManagement;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class Student {
 
+  private String student_id;
   private String name;
+  private String furigana;
+  private String nickname;
+  private String emailAddress;
+  private String residentialArea;
   private int age;
+  private String gender;
 
-  public String getName() {
-    return name;
-  }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
 }
--- src/main/java/raisetech/java/StudentManagement/StudentRepository.java
+++ src/main/java/raisetech/java/StudentManagement/StudentRepository.java
@@ -1,32 +1,16 @@
 package raisetech.java.StudentManagement;
 
-
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM student")
-  List<Student> getStudentList(String name);
+  @Select("SELECT * FROM students WHERE name = #{name}")
+  List<Student> search();
 
-  @Select("SELECT * FROM student")
-  List<Student> getStudentList2(String name);
+  // students_coureseの全件取得を行う。
 
-  @Select("SELECT * FROM student WHERE name = #{name}")
-  Student searchByName(String name);
-
-  @Insert("INSERT student values(#{name},#{age})")
-  void resisterStudent(String name, int age);
-
-  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-  void updateStudent(String name, int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
 
 }
